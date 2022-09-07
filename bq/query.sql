e9a3259db3891991b046639ccee138763588d91d
Piotr Ostrowski <piotrostr@google.com>
2022-09-07 17:40:41 +0100

    feat: add sample query

---
+++ bq/query.sql
@@ -0,0 +1,12 @@
+SELECT
+  ProductTable.product,
+  COUNT(SalesReceipts.product_id) AS totalSold
+FROM
+  `famous-vista-361810.team_mega_dataset.Product` AS ProductTable
+INNER JOIN `famous-vista-361810.team_mega_dataset.SalesReceipts` AS SalesReceipts
+  ON ProductTable.product_id = SalesReceipts.product_id
+GROUP BY
+  ProductTable.product
+ORDER BY
+  totalSoldASC
+LIMIT 10
